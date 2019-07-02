6f0fc88358413483f16516eeae5832817217cb34
y.kazheka <y.kazheka@godeltech.com>
2019-07-02 18:39:06 +0300

    add models

---
+++ Owasp/Owasp.Sqli/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace Owasp.Sqli.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public int ClockNumber { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string JobTitle { get; set; }
+        public string Mobile { get; set; }
+    }
+}
---
+++ Owasp/Owasp.Sqli/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Owasp.Sqli.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
